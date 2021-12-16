7f14e1ad0e2a7ef4db6df03762161d9b6096a619
Ali Abdul <ali.abdulhussein@iver.se>
2021-12-16 08:39:48 +0100

    initial and project stracture

---
+++ Strategy/InvoiceOutputRender/InvoiceOutputRender/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter Type:");
+
+var input = Console.ReadLine();
+input=char.ToUpper(input![0]) +input.Substring(1);
+
+switch (input)
+{
+    case "Pdf":
+        Console.WriteLine($"Type is: {input}");
+        break;
+    case "File":
+        Console.WriteLine($"Type is: {input}");
+        break;
+    case "Console":
+        Console.WriteLine($"Type is: {input}");
+        break;
+    default:
+        break;
+}
+
+Console.ReadLine();
